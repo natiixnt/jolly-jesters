57a13107240d2b99a4950e8af1117ee2334dec4b
nai <81188562+natiixnt@users.noreply.github.com>
2025-10-30 20:46:47 +0100

    Create models.py

---
+++ backend/models.py
@@ -0,0 +1,4 @@
+from sqlalchemy import Column, Integer, String, Numeric, Boolean, ForeignKey, DateTime, UniqueConstraint
+from sqlalchemy.dialects.postgresql import JSONB
+from sqlalchemy.sql import func
+from .databas
